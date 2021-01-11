b4b820956d70c7698422a3c992492564988c192b
Frank-Woods <49346787+Frank-Woods@users.noreply.github.com>
2021-01-11 11:41:40 +0400

    update

--- user-page/src/main/java/ru/fwoods/userpage/MvcConfig.java
+++ user-page/src/main/java/ru/fwoods/userpage/MvcConfig.java
@@ -23,7 +23,7 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
-                .addResourceLocations("localhost:8082/static/");
+                .addResourceLocations("http://localhost:8082/static/");
         registry.addResourceHandler("/image/userImage/**")
                 .addResourceLocations("file://" + userUploadPath + "/");
     }
